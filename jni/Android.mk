fd1c26077ad1059266e30c551e62c1f08e5b21c3
Chad Rockey <chadrockey@willowgarage.com>
2013-11-04 00:48:35 -0800

    updated to build static library.

--- jni/Android.mk
+++ jni/Android.mk
@@ -5,12 +5,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    	:= libtinyxml
 LOCAL_C_INCLUDES	:= $(LOCAL_PATH)/include 
 									
-
-LOCAL_LDLIBS		:= -llog -lz 
 LOCAL_SRC_FILES 	:= src/tinystr.cpp \
 				src/tinyxml.cpp \
 				src/tinyxmlerror.cpp \
 				src/tinyxmlparser.cpp 
 
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
 
